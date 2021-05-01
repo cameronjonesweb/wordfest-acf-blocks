0eaa242079b52fd7b01b395c4315d163feaf1caf
Cameron Jones <cameron@cameronjonesweb.com.au>
2021-05-01 20:02:50 +0930

    ACF blocks basic implementation

--- includes/acf-blocks.php
+++ includes/acf-blocks.php
@@ -1,1 +1,23 @@
 <?php
+
+function wordfest_acf_init_block_types() {
+
+	// Check function exists.
+	if( function_exists('acf_register_block_type') ) {
+
+		// register a testimonial block.
+		acf_register_block_type(
+			array(
+				'name'            => 'testimonial',
+				'title'           => __( 'Testimonial' ),
+				'description'     => __( 'A custom testimonial block.' ),
+				'render_template' => 'template-parts/blocks/testimonial/testimonial.php',
+				'category'        => 'formatting',
+				'icon'            => 'admin-comments',
+				'keywords'        => array( 'testimonial', 'quote' ),
+			)
+		);
+	}
+}
+
+add_action( 'acf/init', 'wordfest_acf_init_block_types' );
